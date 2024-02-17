4347528d1ec5ba5ba648372aa008d50ac3fff817
Msksgm <msksgm3124@gmail.com>
2024-02-17 12:11:58 +0900

    feat: Spring Boot プロジェクトを新規作成

---
+++ src/main/kotlin/com/example/kotlinspringboothandleconstraintviolation/KotlinSpringBootHandleConstraintViolationApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinspringboothandleconstraintviolation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringBootHandleConstraintViolationApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinSpringBootHandleConstraintViolationApplication>(*args)
+}
